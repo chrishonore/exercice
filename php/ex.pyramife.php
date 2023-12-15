63353eeffc0008bd1cea561d7a427864a2008dc9
unknown <christianhonore@outlook.com>
2023-12-15 17:20:30 +0100

    exercice

---
+++ php/ex.pyramife.php
@@ -0,0 +1,9 @@
+<?php
+// realiser une pyramide avec des etoile//
+
+function DisplayPyramid( ) {
+    for ( $i=1;$i<=5;$i++){
+echo "*\n";
+}
+}
+DisplayPyramid();
--- php/exercice.4.class.php
+++
@@ -1,5 +0,0 @@
-<?php
-// l utilisateur dois mettre le jour et mois de naissance
-echo " entrer votre jour et mois de naissance pour connaitre votre signe astrologique " ;
-$date=fgets(STDIN);
-if ( $date )
