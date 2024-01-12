d6689b3744c79e4f48f6630a9fc990e21ebcfb61
unknown <christianhonore@outlook.com>
2024-01-12 09:30:54 +0100

    test

---
+++ php/revision.if.elseif.etc.php
@@ -0,0 +1,7 @@
+<?php
+echo " Bienvenu on va faire un petit test de culture . " ;
+echo " premiere question \n comment  s appele le pere de pinocchio ? ";
+$quest1=trim(ffgets(STDIN));
+echo " deuxieme question \n comment s appele la soeur d anna ? " ;
+$quest2 =trim(ffgets(STDIN));
+?>
