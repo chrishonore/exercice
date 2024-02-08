831163b3d68e84b8387504aff78c00f5f181dc02
unknown <christianhonore@outlook.com>
2024-02-08 23:27:26 +0100

    'formulaire'

--- sql/ex35.sql
+++ sql/ex35.sql
@@ -5,10 +5,12 @@ Elle recevra deux paramètres de type VARCHAR(20).
 Le premier se nommera first_name, le second last_name.
 La fonction retournera par exemple 'Hello Johnny Piette !'
  */
-create function hello (first_name varchar(20), last_name varchar(20))
-returns varchar(50)
-begin
- return 'Piette'|| first_name||'Jhonny'||last_name;
-
-
-
+DROP FUNCTION IF EXISTS Hello ;
+delimiter $$
+CREATE FUNCTION Hello (first_name varchar(20),last_name varchar(20))
+RETURNS  VARCHAR(50)
+BEGIN
+    RETURN concat ('Bonjour ' ,first_name ,' ' , last_name);
+END $$
+delimiter ;
+SELECT  Hello ( 'chris','honore');
